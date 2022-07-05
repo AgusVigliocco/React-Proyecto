e4efa5433e4cfa9ee137f686b59e04937b026585
Agustin Vigliocco <agusvigliocco@icloud.com>
2022-07-05 16:57:06 -0300

    cambio class por className

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,40 +6,40 @@ import React, { Component } from 'react'
 export default class NavBar extends Component {
     render() {
         return (
-            <nav class="navbar navbar-expand-sm navbar-light bg-secondary">
-                <div class="container">
-                    <a class="navbar-brand" href="#">Tienda Deportes</a>
-                    <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+            <nav className="navbar navbar-expand-sm navbar-light bg-secondary">
+                <div className="container">
+                    <a className="navbar-brand" href="#">Tienda Deportes</a>
+                    <button className="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
                         aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+                        <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse mx-5" id="collapsibleNavId">
-                        <ul class="navbar-nav me-auto mt-2 mt-lg-0">
+                    <div className="collapse navbar-collapse mx-5" id="collapsibleNavId">
+                        <ul className="navbar-nav me-auto mt-2 mt-lg-0">
 
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Fútbol</a>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Fútbol</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Básquet</a>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Básquet</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Entrenamiento</a>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Entrenamiento</a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Tiempo Libre</a>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Tiempo Libre</a>
                             </li>
 
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Mas</a>
-                                <div class="dropdown-menu" aria-labelledby="dropdownId">
-                                    <a class="dropdown-item" href="#">Rebajas</a>
-                                    <a class="dropdown-item" href="#">Ultimos talles</a>
+                            <li className="nav-item dropdown">
+                                <a className="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Mas</a>
+                                <div className="dropdown-menu" aria-labelledby="dropdownId">
+                                    <a className="dropdown-item" href="#">Rebajas</a>
+                                    <a className="dropdown-item" href="#">Ultimos talles</a>
                                 </div>
                             </li>
                         </ul>
-                        <form class="d-flex my-2 my-lg-0 ">
-                            <input class="form-control mr-sm-2" type="text" placeholder="Buscar" />
-                            <button class="btn btn-success my-2 my-sm-0 mx-2" type="submit">Buscar</button>
+                        <form className="d-flex my-2 my-lg-0 ">
+                            <input className="form-control mr-sm-2" type="text" placeholder="Buscar" />
+                            <button className="btn btn-success my-2 my-sm-0 mx-2" type="submit">Buscar</button>
                         </form>
                     </div>
                 </div>
